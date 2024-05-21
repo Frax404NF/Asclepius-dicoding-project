ccd064bdf46e675ebf49398aa486ac801daf4710
Frandi <frandicollage21@gmail.com>
2024-05-22 01:37:06 +0800

    add front end

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,5 @@
 // TODO: Silakan sesuaikan BASE URL dari endpoint Anda
-const BASE_URL = 'YOUR_BASE_URL';
+const BASE_URL = 'https://asclepius-kdddadoj6q-et.a.run.app/';
 
 const ENDPOINT = {
   predict: `${BASE_URL}/predict`,
